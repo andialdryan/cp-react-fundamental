e2f5d47eff81c7aa4e1712025d2d090bc12e816f
andialdryan <aldryanmandala@gmail.com>
2024-05-29 08:47:00 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Article from './components/article'
+
+
+function App() {
+
+  return (
+    <>
+      <Article />
+      <Article />
+      <Article />
+      <Article />
+    </>
+  )
+}
+
+export default App
